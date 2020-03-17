86efc1cdf17fc46cbf58e01785e7bf065af0a784
Ubuntu <ubuntu@ip-172-31-34-234.ec2.internal>
2020-03-17 14:01:27 +0000

    Added user online list and username on message

--- index.js
+++ index.js
@@ -26,12 +26,26 @@ io.on('connection', function(socket){
     
     // disconnect
     socket.on('disconnect', function(data){
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s sockets connected', connections.length);
     });
     
     // send message
     socket.on('send message', function(data){
-        io.sockets.emit('new message', {msg: data});
+        io.sockets.emit('new message', {msg: data, user: socket.username});
     });
+    
+    // new user
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+    
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+    }
 });
--- public/chat.js
+++ public/chat.js
@@ -6,14 +6,45 @@ $(function(){
     let $messageForm = $('#messageForm');
     let $message = $('#message');
     let $chat = $('#chat');
+    let $chatArea = $('#chatArea');
+    let $userFormArea = $('#userFormArea');
+    let $userForm = $('#userForm');
+    let $users = $('#users');
+    let $username = $('#username');
     
+    // message form
     $messageForm.submit(function(e){
         e.preventDefault();
         socket.emit('send message', $message.val());
         $message.val('');
     });
     
-    socket.on('new message', function(data){
-        $chat.append('<li id="single-message">' +data.msg+ '</li>');
+    // user form
+    $userForm.submit(function(e){
+        e.preventDefault();
+        socket.emit('new user', $username.val(), function(data){
+            if(data){
+                $userFormArea.hide();
+                $chatArea.show();
+                $chatArea.attr('style', 'display:flex;');
+            }
+        });
+        $username.val('');
     });
+    
+    socket.on('get users', function(data){
+        let online = '';
+        for(i = 0; i < data.length; i++){
+            online += '<li class="list-group-item">' +data[i]+ '</li>';
+        }
+        $users.html(online);
+    });
+    
+    // new message in chat
+    socket.on('new message', function(data){
+        $chat.append('<li id="singleMessage"><strong>' +data.user+ ':' + '</strong> ' +data.msg+ '</li>');
+    });
+    
+    
+    
 });
