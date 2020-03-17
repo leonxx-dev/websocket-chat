58543d23151a58bd8865b5dd021730b96ecb10a5
Ubuntu <ubuntu@ip-172-31-34-234.ec2.internal>
2020-03-17 11:03:28 +0000

    Show connected/disconnected sockets

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ let express = require('express');
 let app = express();
 let server = require('http').createServer(app);
 let io = require('socket.io')(server);
+let port = process.env.PORT || 3000
 
 users = [];
 connections = [];
 
-server.listen(process.env.PORT || 3000);
-console.log('Server running...');
+// runserver
+server.listen(port, function(){
+    console.log('Server running:' + port);
+});
 
+// routes
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
+});
+
+// static files
+app.use(express.static('public'));
+
+io.on('connection', function(socket){
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+    
+    socket.on('disconnect', function(data){
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    });
 });
